48c185c80712a2d71283c04c68950e0ad4c7de4f
jpoveda22 <jpoveda226@gmail.com>
2020-03-20 17:03:00 -0600

    missing navbar but works

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import './App.css';
- 
 import {Switch, Route } from 'react-router-dom'
-import SearchBar from '../SearchBar/SearchBar';
-import SearchResults from '../SearchResults/SearchResults';
-import Playlist from '../Playlist/Playlist';
-import Spotify from '../../util/Spotify';
 import Bumping from '../Bumping/Bumping'
-import Play from '../Play/Play'
 import NavBar from '../NavBar/Navbar';
 import CreateAPlaylist from '../CreateAPlaylist/CreateAplaylist';
 import PlaylistPlayer from '../PlaylistPlayer/PlaylistPlayer';
@@ -17,19 +11,17 @@ class App extends React.Component {
    return (
      <div>
        <div>
-       
- <div className="App">
-   <NavBar />
-   <Switch>
-          <Route exact path='/home' component={Bumping}/> 
-          <Route exact path='/create-a-playlist' component={CreateAPlaylist}/> 
-          <Route exact path='/playlist-player' component={PlaylistPlayer}/>
-        </Switch>
-      
-<div></div>
-    
- </div>
-</div>
+         <div className="App">
+           <NavBar />
+           <Switch>
+             <Route exact path="/home" component={Bumping} />
+             <Route exact path="/create-a-playlist" component={CreateAPlaylist}/>
+             <Route exact path="/playlist-player" component={PlaylistPlayer} />
+           </Switch>
+
+           <div></div>
+         </div>
+       </div>
      </div>
    );
  }
--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -1,5 +1,8 @@
-import React from './node_modules/react';
+import React from 'react';
 import './NavBar.css'
+import Bumping from '../Bumping/Bumping'
+import CreateAPlaylist from '../CreateAPlaylist/CreateAplaylist';
+import PlaylistPlayer from '../PlaylistPlayer/PlaylistPlayer';
 
 
 
@@ -7,9 +10,13 @@ class Navbar extends React.Component {
     render() {
         return (
             <div>
-                 <nav className="NavBar">
-      <span>Bumping</span>
-    </nav>
+                 <navbar className="NavBar">
+      <ul>
+          <li>{<Bumping/>}</li>
+          <li>{<CreateAPlaylist/>}</li>
+          <li>{<PlaylistPlayer/>}</li>
+      </ul>
+  </navbar>
             </div>
         );
     }
